4c3dd480716479b4d9ed0ba45436d1607b973bf8
Selorm Ahorlu <35353089+selormonray@users.noreply.github.com>
2020-08-25 21:30:37 +0000

    added the test for existing email

--- src/test/java/guru/oze/app/SignUp.java
+++ src/test/java/guru/oze/app/SignUp.java
@@ -10,7 +10,7 @@ public class SignUp {
 
     @Test(priority = 5)
     public static void register() {
-        /* create driver */
+//        /* create driver */
 //		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 //		WebDriver driver = new ChromeDriver();
 
@@ -52,9 +52,51 @@ public class SignUp {
         submit.click();
 
 //			verifications
+    }
+//    public static void close() {
+//        driver.quit();
+//    }
+    public static void existingEmail(){
+
+        setUp();
+
+        /* maximize the browser window */
+        driver.manage().window().maximize();
+
+        /* open the web page */
+        String url = ("https://app.oze.guru/");
+        driver.get(url);
+
+        /* click on register */
+        WebElement register = driver.findElement(By.xpath("//a[contains(text(),'Register')]"));
+        register.click();
+
+        /* enter phone number */
+        WebElement phonenumber = driver.findElement(By.xpath("//input[@placeholder='Phone Number']"));
+        phonenumber.sendKeys("542401517");
+
+        /* enter username */
+        WebElement username = driver.findElement(By.name("name"));
+        username.sendKeys("selormonray");
+        /* enter email */
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("selormonray14@gmail.com");
+
+        /* enter password */
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("Zx3zQzHGxeLmKmk");
+
+        /* confirm password */
+        WebElement passwordConfirm = driver.findElement(By.id("password-confirm"));
+        passwordConfirm.sendKeys("Zx3zQzHGxeLmKmk");
+
+        /* register by clicking on next */
+        WebElement submit = driver
+                .findElement(By.xpath("//button[@class='btn btn-primary btn-rounded btn waves-effect waves-light']"));
+        submit.click();
+
+//        verification
 
     }
-    public static void close() {
-        driver.quit();
-    }
+
 }
